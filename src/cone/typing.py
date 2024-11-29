57c6075ac7f7cc3f45f763c134701e694095b962
Roland DENIS <denis@math.univ-lyon1.fr>
2024-11-29 09:31:19 +0100

    Removing TypeAlias to (try to) get compatibility with Python < 3.10

--- src/cone/typing.py
+++ src/cone/typing.py
@@ -1,6 +1,6 @@
-from typing import TypeVar, TypeAlias, Generic, Optional, Iterable, Iterator, TypeGuard
+from typing import TypeVar, Generic, Optional, Iterable, Iterator, TypeGuard
 from typing import cast, overload
-from typing import Any
+from typing import Any, Self
 from collections.abc import Sequence, MutableSequence, Sized, Callable
 
 T = TypeVar('T') # Template parameter for generic classes or functions
