9f98fb75cd4e182e64c8a2449b2b98fb92281a3c
Michael Bulois <michael.bulois@univ-st-etienne.fr>
2024-12-13 17:37:13 +0100

    rajout du fichier Normaliz2python destiné a faire des tests

---
+++ src/cone/Normaliz2python.py
@@ -0,0 +1,68 @@
+from .typing import *
+from re import split as re_split
+from .tau import Tau
+
+def convert_lines_Nout2pyth(lines:Sequence[str],d:"Dimension") -> Sequence[Tau]:
+    """ lines is a list of lines read from a Normaliz output file, e.g. via
+    fichier = open("/home/bm29130h/Documents/Recherche/Ressources_autres/GDT/Machine Learning/calculs Kron/2 oct/ineq_Normaliz"+st+".out","r")
+    lines = fichier.readlines()
+    fichier.close() 
+    
+    They are converted to 1 parameter compatible with our implementation. 
+    """
+    res=[]
+    concerned_line=False
+    for l in lines[16:]:
+        if l=='\n':
+            concerned_line=False
+        if concerned_line:
+            mp=[]
+            m=re_split(r" ",l)
+            m[-1]=m[-1][:-1]
+            mp=[]
+            for mm in m:
+                if mm!='':
+                   mp.append(int(mm))
+            res.append(Tau.from_flatten(mp,d))
+        if l[-21:]=="support hyperplanes:\n":
+            concerned_line=True
+    return(res)
+
+def convert_file_Nout2pyth(rep_path:str, d:"Dimension") -> Sequence[Tau]:
+    """rep_path is the path to the repository containing a Normaliz output, d is the dimension under concern (typically, a given repository can contain many Normaliz outputs for differents dimension d. e.g. path="/home/bm29130h/Documents/Recherche/Ressources_autres/GDT/Machine Learning/calculs Kron/2 oct/"
+    
+    the function then applies convert_lines_Nout2pyth
+    """
+    st=""
+    for di in d:
+       st+="-"+str(di)
+    fichier = open(rep_path+"ineq_Normaliz"+st+".out","r")
+    lines = fichier.readlines()
+    fichier.close() 
+    return(convert_lines_Nout2pyth(lines,d))
+    
+def Compare_tau_candidates_reference(Candidates:Sequence[Tau],Reference:Sequence[Tau])->dict[str,Sequence[Tau]]:
+    res = {key: [] for key in ["cand_only","ref_only","both"]}
+    for i,eq in enumerate(Candidates):
+        if eq.opposite in Reference:
+            res["both"].append(i)
+        else:
+            res["cand_only"].append(i)
+    print(len(res["cand_only"]), "irredundant inequalities found in given list")
+    print(len(res["both"]), "redundant inequalities found in given list")
+    return(res)
+
+
+
+#ineq_normaliz=convertre(lines)
+
+#ineq_normaliz_SL=[tau2SL(ineq,d0) for ineq in ineq_normaliz] 
+
+#ineq
+
+
+
+
+
+
+
