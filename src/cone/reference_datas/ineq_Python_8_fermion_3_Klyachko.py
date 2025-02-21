3bd74ad9483fa45b572789a2e11c87008d52b1a6
Michael Bulois <michael.bulois@univ-st-etienne.fr>
2025-02-21 17:35:50 +0100

    reference datas previously forgotten

---
+++ src/cone/reference_datas/ineq_Python_8_fermion_3_Klyachko.py
@@ -0,0 +1,47 @@
+#Inequalities selected for V of boson type with dimensions [8]with number of particules = 3
+ 
+from ..linear_group import LinearGroup
+from ..representation import *
+from ..inequality import Inequality
+from ..tau import Tau
+
+G=LinearGroup([8]) 
+V= FermionRepresentation(G, particle_cnt=3 )
+ 
+brut_inequations=[(2, -1, -1, -1, -1, -1, -1, 2), 
+(-2, 1, 1, 1, 1, -2, -2, -2), 
+(1, 1, -2, -2, 1, 1, -2, -2), 
+(1, 1, -2, 1, -2, -2, 1, -2), 
+(1, -2, 1, 1, -2, 1, -2, -2), 
+(5, 2, -7, -4, -1, -4, -1, 2), 
+(5, -4, -1, -4, -1, 2, -7, 2), 
+(-1, -1, 2, 5, -7, -4, -4, 2), 
+(2, 5, -7, -1, -4, -4, -1, 2), 
+(5, -4, -1, 2, -7, -4, -1, 2), 
+(1, 1, -2, -1, -1, 0, 0, 0), 
+(1, -1, -1, 0, 0, 1, -2, 0), 
+(1, 0, -1, 0, -1, 0, -1, 0), 
+(0, 0, 0, 1, -1, -1, -1, 0), 
+(0, 1, -1, 0, 0, -1, -1, 0), 
+(1, 0, -1, -1, -1, 0, 0, 1), 
+(1, -1, -1, 0, 0, 0, -1, 1), 
+(2, -1, -1, -1, -1, 2, -4, -1), 
+(2, -1, -1, 2, -4, -1, -1, -1), 
+(2, 2, -4, -1, -1, -1, -1, -1), 
+(-1, -1, 2, 2, -1, -1, -4, -1), 
+(-1, 2, -1, 2, -1, -4, -1, -1), 
+(-1, 2, -1, -1, 2, -1, -4, -1), 
+(-2, 1, 1, 4, -5, -2, -5, 1), 
+(1, 4, -5, 1, -5, -2, -2, 1), 
+(1, -2, 1, 4, -5, -5, -2, 1), 
+(1, 4, -5, -2, 1, -5, -2, 1), 
+(2, 1, -3, -2, -1, -1, 0, 1), 
+(2, -2, -1, -1, 0, 1, -3, 1), 
+(1, 2, -3, -1, -2, -1, 0, 1), 
+(-1, 0, 1, 2, -3, -2, -1, 1), 
+ ] 
+
+#inequalities in our formated type Inequality 
+inequalities_K38=[Inequality.from_tau(Tau.from_flatten(brut_ineq,G)) for brut_ineq in brut_inequations] 
+ 
+ 
---
+++ src/cone/reference_datas/ineq_Python_8_fermion_4_Klyachko.py
@@ -0,0 +1,29 @@
+#Inequalities selected for V of boson type with dimensions [8]with number of particules = 4
+ 
+from ..linear_group import LinearGroup
+from ..representation import *
+from ..inequality import Inequality
+from ..tau import Tau
+
+G=LinearGroup([8]) 
+V = FermionRepresentation(G, particle_cnt=4 )
+ 
+brut_inequations=[(1, 0, 0, -1, 0, -1, -1, 0), 
+(0, 0, 1, -1, 0, 0, -1, -1), 
+(0, 0, 0, 0, 1, -1, -1, -1), 
+(0, 1, 0, -1, 0, -1, 0, -1), 
+(1, -1, 0, 0, 0, 0, -1, -1), 
+(1, 0, 0, -1, -1, 0, 0, -1), 
+(1, 0, -1, 0, 0, -1, 0, -1), 
+(-1, 1, 1, -1, 1, -1, -1, -3), 
+(1, 1, -1, -1, 1, -3, -1, -1), 
+(1, 1, 1, -3, -1, -1, -1, -1), 
+(1, -1, 1, -1, 1, -1, -3, -1), 
+(1, 1, -1, -1, -1, -1, 1, -3), 
+(1, -1, -1, 1, 1, -1, -1, -3), 
+(1, -1, 1, -1, -1, 1, -1, -3), 
+(3, -1, -1, -1, -1, -1, -1, -1), 
+ ] 
+
+#inequalities in our formated type Inequality 
+inequalities_K48=[Inequality.from_tau(Tau.from_flatten(brut_ineq,G)) for brut_ineq in brut_inequations]
