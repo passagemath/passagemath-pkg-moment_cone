6f1146cfd57acd63aee402575ca75cc7a304febc
Roland DENIS <denis@math.univ-lyon1.fr>
2025-03-06 13:43:54 +0100

    Adding some missing import in __init__

--- src/cone/__init__.py
+++ src/cone/__init__.py
@@ -5,8 +5,9 @@ from .weight import *
 from .representation import *
 from .linear_group import *
 from .root import *
-
 from .tau import Tau
 from .inequality import Inequality
+from .main_steps import *
+from .main import *
 
 # FIXME: remaining default imports
